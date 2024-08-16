c64617197be0846447ec8b1e75554490c4cc7b58
Dammala Uday Kiran <106368081+the-silent-geek@users.noreply.github.com>
2024-08-17 02:04:29 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,12 +3,12 @@ import joblib
 
 model1 = joblib.load("model.pkl")
 
-
-def pred(days, base_series1):
+def pred(days, base_series1, test):
   
   for i in range(0,days):
     temp = model1.predict(base_series1)
     base_series1 = np.append(base_series1, temp[0][0])
     base_series1 = base_series1[1:].reshape(1,50,1)
+    test = np.append(test,temp[0][0])
 
-  return base_series1
+  return test
